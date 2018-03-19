9a9eb5d48c73bcc165f3acb5784c9aa589c4ab72
cupshir <christopher.upshir@gmail.com>
2018-03-18 17:43:44 -0700

    Journal now displays a list of journal entries for the active journal. Added search functionality to the journal entries list. Changed list to be a sectionlist instead of a flatlist. Updated to use new user schema (from combining journal into user). Im sure Im forgetting other changes...was a lot...

--- src/screens/Journal/Journal.js
+++ src/screens/Journal/Journal.js
@@ -2,21 +2,28 @@ import React, { Component } from "react";
 import { 
   View, 
   Text,
-  FlatList, 
+  Button,
+  FlatList,
+  SectionList, 
   AlertIOS, 
   StyleSheet 
 } from "react-native";
+import {
+  SearchBar
+} from 'react-native-elements';
 import { connect } from 'react-redux';
-import * as journalActions from '../../store/actions/journalActions';
+import moment from 'moment';
+import * as userActions from '../../store/actions/userActions';
 
-import ListItem from "../../components/ListItem";
+import ListItemAttraction from "../../components/ListItemAttraction";
+import SectionHeader from "../../components/SectionHeader";
 import LoadingMickey from '../../components/LoadingMickey';
 
 class Journal extends Component {
-    static navigatorButtons = {
+  static navigatorButtons = {
       rightButtons: [
         {
-          title: '+',
+          //title: '+',
           id: 'addEntry'
         }
       ],
@@ -31,6 +38,7 @@ class Journal extends Component {
   constructor(props) {
     super(props);
     this.state = { 
+      filteredEntries: null,
       isLoading: false
     };
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
@@ -39,15 +47,10 @@ class Journal extends Component {
   onNavigatorEvent(event) {
     if (event.type == 'NavBarButtonPress') {
       if (event.id == 'journals') {
-        this.props.navigator.push({
-          screen: 'vacationjournalios.JournalList',
-          title: 'Journals',
-          animated: true,
-          animationType: 'fade'
-        })
+        this.onJournalListPress();
       }
       if (event.id == 'addEntry') {
-        if (this.props.journal.journal) {
+        if (this.props.user.activeJournal) {
           this.props.navigator.push({
             screen: 'vacationjournalios.CreateJournalEntry',
             title: 'Add Journal Entry',
@@ -59,6 +62,35 @@ class Journal extends Component {
         }
       }
     }
+  }
+
+  componentDidUpdate() {
+    // Update screen title
+    if (this.props.user.activeJournal) {
+      this.props.navigator.setTitle({
+        title: (this.props.user.activeJournal ? this.props.user.activeJournal.name : 'Select A Journal')
+      });
+    }
+  }
+
+  // Hack to make nav bar look like native IOS with search under title without border
+  componentDidMount() {
+    this.props.navigator.setStyle({
+      navBarNoBorder: true
+    });
+
+    
+  }
+
+  // navigate to journal list screen
+  onJournalListPress = () => {
+    this.props.navigator.push({
+      screen: 'vacationjournalios.JournalList',
+      title: 'Journals',
+      backButtonTitle: '',
+      animated: true,
+      animationType: 'fade'
+    })
   }
 
   // row on press event
@@ -73,7 +105,7 @@ class Journal extends Component {
 
   // row delete press event
   onDeletePress = (id) => {
-    // display confirm prompt, user must type matching name to delete
+    // display confirm prompt, user must type CONFIRM to delete
     AlertIOS.prompt(
       'Confirm Delete',
       'Type CONFIRM (all caps) to proceed with deletion',
@@ -91,33 +123,125 @@ class Journal extends Component {
   }
 
   // handle delete journal entry
-  handleDeleteJournal = (id, enteredText) => {
+  handleDeleteJournalEntry = (id, enteredText) => {
     // verify typed text is CONFIRM
     if(enteredText === 'CONFIRM' ) {
       // match, delete journal by id
-      this.props.dispatch(journalActions.deleteJournalEntry(id));
+      this.props.dispatch(userActions.deleteJournalEntry(id));
     } else {
       // dont match, display alert and do nothing
       AlertIOS.alert('Incorrect CONFIRM text entered. Journal entry not deleted!')
     }
   }
 
+  handleSearch = (searchInput) => {
+    // create filter object from all journal entries in props
+    let filteredEntries = this.props.user.activeJournal.journalEntries;
+
+    // if search input contains text, filter by that text
+    if (searchInput !== '') {      
+      filteredEntries = this.props.user.activeJournal.journalEntries.filtered('attraction.name CONTAINS[c] $0 || park.name CONTAINS[c] $0', searchInput);
+    }
+
+    // Save the updated filtered into state
+    this.setState({
+      ...this.state,
+      filteredEntries: filteredEntries
+    });
+  }
+
   // render row item
   renderItem = ({ item }) => (
-    <ListItem
-      id={item.id}
+    <ListItemAttraction
+      item={item}
       onPress={this.onPress}
       onEditPress={this.onEditPress}
-      onDeletePress={this.onDeletePress}
-      title={item.name}
-      viewStyle={styles.listView}
-      textStyle={styles.listText}
-    />
+      onDeletePress={this.onDeletePress} />
   );
-  
-  
+
+  // render Header row
+  renderSectionHeader = ({ section }) => (
+    <SectionHeader
+      title={section.title} />
+  );
+
+  // Render Search Input (placed here to keep view below easier to read)
+  renderSearchInput = () => {
+    return (
+      <SearchBar
+        platform='ios'
+        lightTheme
+        containerStyle={styles.searchContainer}
+        inputStyle={styles.searchInput}
+        onChangeText={this.handleSearch}
+        icon={{
+          style: {
+            marginLeft: 4
+          }
+        }}
+        clearIcon={{
+          name: 'close'
+        }}
+        placeholder='Search' />
+    );
+  }
+
+  // return list of unique dates in journal entries
+  getListOfUniqueDates = (journalEntries) => {
+    let returnList = [];
+
+    // loop through each entry and if dateJournaled does not exist in return array add it
+    journalEntries.forEach(entry => {
+      const date = moment(entry.dateJournaled).format("MM/DD/YYYY");
+      if (!returnList.includes(date)) {
+        const dateObject = [ date, entry.dateJournaled ];
+        returnList.push(date);
+      }
+    });
+
+    // return the list sorted descending
+    return returnList.sort().reverse();
+  }
+
+  // split journal entries array into sections
+  splitJournalEntriesIntoSections = (journalEntries) => {
+    // Get section headers
+    const sectionHeaders = this.getListOfUniqueDates(journalEntries);
+
+    let returnArray = [];
+
+    // for each section header create a filtered array
+    for (i = 0; i < sectionHeaders.length; i++) {
+      // return objects
+      let array = [];
+      
+      // add title
+      const title = sectionHeaders[i];
+      array.push(title);
+
+      // build data
+      // start date for filter...midnight of date. x/x/xxxx 00:00
+      const dateStart = new Date(sectionHeaders[i]);
+      // create end date, which is next day with time of midnight
+      const dateEnd = new Date(sectionHeaders[i]);
+      dateEnd.setDate(dateEnd.getDate() + 1);
+
+      // Filter journal entries using start time and end time of current section
+      const data = journalEntries.filtered('dateJournaled >= $0 && dateJournaled < $1', dateStart, dateEnd);
+
+      // add data
+      array.push(data);
+
+      // add array to return array
+      returnArray.push(array);
+    }
+
+    return returnArray;
+  }
+
   render() {
-    if (this.state.isLoading) {
+    // TODO: This isnt working, figure out why...
+    if (this.props.user.status === 'saving') {
       return (
         <View style={styles.container}>
           <LoadingMickey />
@@ -125,26 +249,38 @@ class Journal extends Component {
       );
     }
 
-    if (!this.props.journal.journal) {
+    // Check for active Journal
+    if (!this.props.user.activeJournal) {
       return (
         <View style={styles.container}>
-        <Text>select journal button</Text>
-      </View>
+          <Button title='Select A Journal' onPress={this.onJournalListPress} />
+        </View>
       );
     }
 
+    // build search input
+    const searchInput = this.renderSearchInput();
+
+    // load journalEntries from props if filteredEntries in state is null (this is so something loads when view first renders) 
+    const filteredEntries = this.state.filteredEntries !== null ? this.state.filteredEntries : this.props.user.activeJournal.journalEntries;
+
+    // get section headers of filtered entries
+    //const sectionHeaders = this.getListOfUniqueDates(filteredEntries);
+
+    // Split filtered array into sections array
+    const journalEntriesBySection = this.splitJournalEntriesIntoSections(filteredEntries);
+
     return (
       <View>
-        <Text>This will be journal page.</Text>
-        <Text>Journal Name: {this.props.journal.journal ? this.props.journal.journal.name : null}</Text>
-        <Text>Selected Journal is {this.props.journal.journal ? this.props.journal.journal.id : null}</Text>
-        <Text>Journal Entries...</Text>
-        <View style={styles.container}>
-          <FlatList
-            data={this.props.journal.journal.entries}
+        {searchInput}
+        <View style={styles.sectionContainer}>
+          <SectionList
+            sections={journalEntriesBySection.map(function(object, i) {
+              return { title: object[0], data: object[1] };
+            })}
             renderItem={this.renderItem}
-            keyExtractor={item => item.id}
-            />
+            renderSectionHeader={this.renderSectionHeader}
+            keyExtractor={ (item, index) => index } />
         </View>
       </View>
     );
@@ -157,23 +293,27 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  listView: {
-    padding: 5,
-    borderBottomColor: 'lightgrey',
-    borderBottomWidth: 1,
-    width: 300
+  searchContainer: {
+    backgroundColor: 'white',
+    alignSelf: 'stretch',
+    borderTopColor: 'white',
+    marginTop: -10
   },
-  listText: {
-    color: 'blue',
-    textAlign: 'center',
-    fontSize: 20
+  sectionContainer: {
+    marginBottom: 100
+  },
+  searchInput: {
+    backgroundColor: '#EEEEEE',
+    color: 'black',
+    borderRadius: 5,
+    marginLeft: 15,
+    marginRight: 15
   }
 })
 
 function mapStateToProps(state) {
   return {
-    user: state.user,
-    journal: state.journal,
+    user: state.user
   }
 }
 
