023afe6a496341adae2cfa09e9c4b0b5e56958c8
cupshir <christopher.upshir@gmail.com>
2018-03-18 17:38:23 -0700

    Custom Section Header for SectionList componenet

---
+++ src/components/SectionHeader.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from "react";
+import { 
+    View, 
+    Text, 
+    StyleSheet
+} from "react-native";
+import PropTypes from 'prop-types';
+
+// SectionHeader for SectionList
+class SectionHeader extends PureComponent {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    {this.props.title}
+                </Text>
+            </View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#DDDDDD',
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 15,
+        paddingRight: 15
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+SectionHeader.propTypes = {  
+    title: PropTypes.string.isRequired
+};
+
+export default SectionHeader;
